b04a26376c77d56eea5ee2ad4f70c4b216dfaa4b
DuongQuocVuong97 <38318609+DuongQuocVuong97@users.noreply.github.com>
2022-07-21 17:15:57 +0700

    Add files via upload

--- models/crm_customer_request.py
+++ models/crm_customer_request.py
@@ -12,6 +12,5 @@ class crm_customer_request(models.Model):
     product_id = fields.Many2one('product.template', required=True, index=True)
     date = fields.Date(string="Date", default=fields.Date.today(), required=True)
     qty = fields.Float('Quantity', required=True, digits='Product UoS', default=1)
-    total_qty = fields.Many2many('sale.order.line')
     qty_ordered = fields.Float(string="Quantity Ordered", compute='_qty_ordered')
 
